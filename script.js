241e8a7fa145045b628873adfc1da018e12e7dc9
Edier Andrés Villaneda Navarro <ediervillaneda@gmail.com>
2020-11-04 20:07:58 -0500

    Uri models

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('https://ediervillaneda.github.io/faceDetector/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('https://ediervillaneda.github.io/faceDetector/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('https://ediervillaneda.github.io/faceDetector/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('https://ediervillaneda.github.io/faceDetector/models')
 ]).then(startVideo)
 
 function startVideo() {
